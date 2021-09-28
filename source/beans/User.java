25c21e77790b7019ef254f4669c41fb3f10ad75e
Eric Jones <eric70x7@gmail.com>
2021-09-27 17:28:54 -0700

    Fixed compile/runtime version mismatch

--- source/beans/User.java
+++ source/beans/User.java
@@ -1,9 +1,15 @@
 package beans;
 
-// import javax.faces.bean.*;
+import javax.faces.bean.*;
 
-// @ManagedBean
-// @SessionScoped
+// With @ManagedBean, this was failing with the runtime error 
+// beans/User has been compiled by a more recent version of the Java Runtime (class file version 53.0), 
+// this version of the Java Runtime only recognizes class file versions up to 52.0
+//
+// Because for some reason the compiler compliance level was set to 9 in the PROJECT SPECIFIC settings
+
+@ManagedBean
+@SessionScoped
 public class User {
 	
 	String firstName;
